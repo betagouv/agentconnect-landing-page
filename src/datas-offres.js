45201eec5200cda4f4633780dcd5d8ee76b6dd8f
rebeccadumazert <rebeccadumazert@hotmail.fr>
2023-06-05 10:55:05 +0200

    refactore : - déclaration des routes - les offres page dnum

---
+++ src/datas-offres.js
@@ -0,0 +1,34 @@
+exports.offres=[ {title: "OSMOSE" ,
+    url: "https://osmose.numerique.gouv.fr/plugins/DINUMThemePlugin/jsp/displayPage.jsp?id=c_2251950" ,
+    description: "La plateforme collaborative des communautés professionnelles de l’État" }, {title: "RESANA" ,
+    url: "https://resana.numerique.gouv.fr/public/utilisateur/afficherPageConnexionAnnexe?page=apropos" ,
+    description: "Groupes de travail et suite collaborative en ligne" }, {title: "WebConf" ,
+    url: "https://webconf.numerique.gouv.fr/apropos" ,
+    description: "Audio, vidéo, chat, partage d'écran et de documents" }, {title: "Webinaire" ,
+    url: "https://webinaire.numerique.gouv.fr/documentation" ,
+    description: "Le service de séminaires en ligne pour les agents de l'État" }, {title: "Démarches Simplifiées" ,
+    url: "https://doc.demarches-simplifiees.fr/" , description: "Effectuer une démarche administrative en ligne" },
+    {title: "RIZOMO" , url: "https://rizomo.numerique.gouv.fr/signin" ,
+    description: "Un raccourci vers les applications collaboratives interministérielles" }, {title: "SILL" ,
+    url: "https://sill.code.gouv.fr/readme" ,
+    description: "Catalogue de référence de logiciels libres recommandés pour l'ensemble de l'administration" },
+    {title: "France Transfert" , url: "https://francetransfert.numerique.gouv.fr/upload" ,
+    description: "Envoyer des fichiers volumineux non sensibles de manière sécurisée à un agent de l'Etat ou entre agents"
+    }, {title: "MOBILIC" , url: "https://mobilic.beta.gouv.fr/resources/home" ,
+    description: "La plateforme numérique gouvernementale de suivi du temps de travail dans le transport routier léger"
+    }, {title: "DATALAB" , url: "https://docs.sspcloud.fr/" ,
+    description: "Travaillez avec Python ou R et disposez de la puissance dont vous avez besoin" }, {title: "ORION" ,
+    url: "https://orion.cerema.fr/auth/realms/CeremaApps/account/#/" , description: "Gestion de compte Keycloak" },
+    {title: "Expertises Territoires" ,
+    url: "https://www.expertises-territoires.fr/jcms/jca_6433/fr/communaute-a-la-une-sante-environnement" ,
+    description: "Faire connaître ses actions, engager des collaborations, partager des ressources & questionnements" },
+    {title: "MENTOR" , url: "https://mentor.gouv.fr/local/staticpage/view.php?page=ensavoirplus" ,
+    description: "La plateforme interministérielle de formation" }, {title: "CISIRH" ,
+    url: "https://portail.cisirh.rie.gouv.fr/" ,
+    description: "Portail des applications interministérielles RH (fonctionne uniquement sur le RIE)" } ] 
+
+
+exports.offres_test=[ 
+    {title: "E-Campus", description: "Plateforme de formation professionnelle pour les agents du Ministère de l'Intérieur" }, 
+    {title: "Contrats Territoires", description: "Service pour élaborer les Contrats Plan État-Région (CPER)" },
+    {title: "SPOTE", description: "Plateforme numérique collaborative des offres numériques de l'État" } ]
