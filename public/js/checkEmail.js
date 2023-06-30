8051955172463909453e4cab40b096b678a6ab88
rebeccadumazert <rebeccadumazert@hotmail.fr>
2023-06-30 17:02:51 +0200

    mise à jour de l'encart éligibilité

--- public/js/checkEmail.js
+++ public/js/checkEmail.js
@@ -1,45 +1,24 @@
 (function(){
     window.addEventListener("load", function(){
-        var isEligible = ""
-        const eligibility_toggle = document.getElementById("toggle-698");
-        eligibility_toggle.addEventListener('click', (e) => {
-            console.log(eligibility_toggle.checked)
-            eligibility_toggle.checked ? isEligible = true : isEligible = false
-        } )
-        const btn = document.getElementById("checkEmail");
-        btn.addEventListener('click', (e) => {   
-            const emailValue = document.getElementById('emailValue').value
-            if (emailValue === "") {
-                document.getElementById("fillYourEmail")
+        var yes = document.querySelector('input[value="yes"]');
+        var no = document.querySelector('input[value="no"]');
+
+        yes.addEventListener('change', function() {
+            if (this.checked) {
+              document.getElementById("eligible")
+              .classList.remove("js-eligibility-hidden")
+              document.getElementById("eligibilityCheck")
+              .classList.add("js-eligibility-hidden")
+            }
+          });
+
+          no.addEventListener('change', function() {
+            if (this.checked) {
+                document.getElementById("notEligible")
                 .classList.remove("js-eligibility-hidden")
-                return;
+                document.getElementById("eligibilityCheck")
+                .classList.add("js-eligibility-hidden")
             }
-            if (isEligible) {
-                document
-                    .getElementById("eligibility-wrapper").
-                    classList.remove("js-eligibility-hidden")
-                document
-                    .getElementById("eligible")
-                    .classList.remove("js-eligibility-hidden")
-                document
-                    .getElementById("eligibility_check")
-                    .classList.add("js-eligibility-hidden")
-                document
-                    .getElementById("notEligible")
-                    .classList.add("js-eligibility-hidden")
-            } 
-            else {
-                document
-                    .getElementById("eligibility-wrapper").
-                    classList.remove("js-eligibility-hidden")
-                document
-                    .getElementById("notEligible")
-                    .classList.remove("js-eligibility-hidden")
-                document
-                    .getElementById("eligibility_check")
-                    .classList.add("js-eligibility-hidden")
-            }
-        }) 
-        
+          });  
     })
 }())
