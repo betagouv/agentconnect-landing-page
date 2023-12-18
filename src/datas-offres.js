3f8724c9d3110460b71067f0c13cda0dd2ca5fe6
Douglas DUTEIL <douglasduteil@gmail.com>
2023-12-18 16:10:34 +0100

    run prettier on all the files

--- src/datas-offres.js
+++ src/datas-offres.js
@@ -1,50 +1,149 @@
-exports.offres_fi=[ {title: "Osmose" ,
-    url: "https://osmose.numerique.gouv.fr/plugins/DINUMThemePlugin/jsp/front/privateLoginOsmose.jsp?redirect=https%3A%2F%2Fosmose.numerique.gouv.fr%2Fjcms%2F" ,
-    description: "Outil de travail collaboratif" }, {title: "Resana" ,
-    url: "https://resana.numerique.gouv.fr/public/" ,
-    description: "Outil de travail collaboratif" }, {title: "Webconférence de l’État" ,
-    url: "https://webconf.numerique.gouv.fr/" ,
-    description: "Outil de conférence à distance" }, {title: "Webinaire" ,
-    url: "https://webinaire.numerique.gouv.fr/welcome" ,
-    description: "Outil de conférence à distance" }, {title: "Démarches Simplifiées" ,
-    url: "https://www.demarches-simplifiees.fr/" , description: "Outil pour réaliser des démarches administratives en ligne" },
-    {title: "Rizomo" , url: "https://rizomo.numerique.gouv.fr/" ,
-    description: "Portail / raccourci vers des applications collaboratives interministérielles" }, {title: "Socle interministériel de logiciels libres" ,
-    url: "https://sill.code.gouv.fr/" ,
-    description: "Catalogue de référence des logiciels libres au sein de l’État" },
-    {title: "France Transfert" , url: "https://francetransfert.numerique.gouv.fr/connect",
-    description: "Envoi de fichiers volumineux non sensibles de manière sécurisée à un agent de l’État ou entre agent"
-    }, {title: "Mobilic" , url: "https://mobilic.beta.gouv.fr/" ,
-    description: "Plateforme numérique gouvernementale de suivi du temps de travail dans le transport routier léger"
-    }, {title: "Datalab SSP Cloud" , url: "https://datalab.sspcloud.fr/home)" ,
-    description: "Plateforme mutualisée de services de traitement des données statistiques et de datascience" }, {title: "Orion du CEREMA Internet" ,
-    url: "https://orion.cerema.fr/auth/realms/CeremaApps/account/#/" , description: "Services du CEREMA (centre d’études et d’expertise sur les risques, l’environnement, la mobilité et l’aménagement)" },
-    {title: "Orion du CEREMA RIE" ,
-    url: "https://orion.cerema.e2.rie.gouv.fr/auth/realms/CeremaApps/account/#/" , description: "Services du CEREMA (centre d’études et d’expertise sur les risques, l’environnement, la mobilité et l’aménagement)" },
-    {title: "Expertises Territoires" ,
-    url: "https://www.expertises-territoires.fr/" ,
-    description: "Services du CEREMA (centre d’études et d’expertise sur les risques, l’environnement, la mobilité et l’aménagement)" },
-    {title: "Mentor" , url: "https://mentor.gouv.fr/login/index.php" ,
-    description: "Plateforme interministérielle de formation professionnelle" }, {title: "Portail CISIRH RIE" ,
-    url: "https://portail.cisirh.rie.gouv.fr/" ,
-    description: "Portail des applications interministérielles RH" },
-    {title: "Contrats Territoires Internet et RIE", url: "https://contrats-territoires.interieur.gouv.fr/accounts/login", description: "Service pour élaborer les Contrats Plan État-Région (CPER)" }
- ] 
+exports.offres_fi = [
+  {
+    title: "Osmose",
+    url: "https://osmose.numerique.gouv.fr/plugins/DINUMThemePlugin/jsp/front/privateLoginOsmose.jsp?redirect=https%3A%2F%2Fosmose.numerique.gouv.fr%2Fjcms%2F",
+    description: "Outil de travail collaboratif",
+  },
+  {
+    title: "Resana",
+    url: "https://resana.numerique.gouv.fr/public/",
+    description: "Outil de travail collaboratif",
+  },
+  {
+    title: "Webconférence de l’État",
+    url: "https://webconf.numerique.gouv.fr/",
+    description: "Outil de conférence à distance",
+  },
+  {
+    title: "Webinaire",
+    url: "https://webinaire.numerique.gouv.fr/welcome",
+    description: "Outil de conférence à distance",
+  },
+  {
+    title: "Démarches Simplifiées",
+    url: "https://www.demarches-simplifiees.fr/",
+    description: "Outil pour réaliser des démarches administratives en ligne",
+  },
+  {
+    title: "Rizomo",
+    url: "https://rizomo.numerique.gouv.fr/",
+    description:
+      "Portail / raccourci vers des applications collaboratives interministérielles",
+  },
+  {
+    title: "Socle interministériel de logiciels libres",
+    url: "https://sill.code.gouv.fr/",
+    description:
+      "Catalogue de référence des logiciels libres au sein de l’État",
+  },
+  {
+    title: "France Transfert",
+    url: "https://francetransfert.numerique.gouv.fr/connect",
+    description:
+      "Envoi de fichiers volumineux non sensibles de manière sécurisée à un agent de l’État ou entre agent",
+  },
+  {
+    title: "Mobilic",
+    url: "https://mobilic.beta.gouv.fr/",
+    description:
+      "Plateforme numérique gouvernementale de suivi du temps de travail dans le transport routier léger",
+  },
+  {
+    title: "Datalab SSP Cloud",
+    url: "https://datalab.sspcloud.fr/home)",
+    description:
+      "Plateforme mutualisée de services de traitement des données statistiques et de datascience",
+  },
+  {
+    title: "Orion du CEREMA Internet",
+    url: "https://orion.cerema.fr/auth/realms/CeremaApps/account/#/",
+    description:
+      "Services du CEREMA (centre d’études et d’expertise sur les risques, l’environnement, la mobilité et l’aménagement)",
+  },
+  {
+    title: "Orion du CEREMA RIE",
+    url: "https://orion.cerema.e2.rie.gouv.fr/auth/realms/CeremaApps/account/#/",
+    description:
+      "Services du CEREMA (centre d’études et d’expertise sur les risques, l’environnement, la mobilité et l’aménagement)",
+  },
+  {
+    title: "Expertises Territoires",
+    url: "https://www.expertises-territoires.fr/",
+    description:
+      "Services du CEREMA (centre d’études et d’expertise sur les risques, l’environnement, la mobilité et l’aménagement)",
+  },
+  {
+    title: "Mentor",
+    url: "https://mentor.gouv.fr/login/index.php",
+    description: "Plateforme interministérielle de formation professionnelle",
+  },
+  {
+    title: "Portail CISIRH RIE",
+    url: "https://portail.cisirh.rie.gouv.fr/",
+    description: "Portail des applications interministérielles RH",
+  },
+  {
+    title: "Contrats Territoires Internet et RIE",
+    url: "https://contrats-territoires.interieur.gouv.fr/accounts/login",
+    description: "Service pour élaborer les Contrats Plan État-Région (CPER)",
+  },
+];
 
+exports.offres_fi_test = [
+  {
+    title: "E-Campus",
+    description:
+      "Plateforme de formation professionnelle pour les agents du Ministère de l'Intérieur",
+  },
+  {
+    title: "SPOTE",
+    description:
+      "Plateforme numérique collaborative des offres numériques de l'État",
+  },
+];
 
-exports.offres_fi_test=[ 
-    {title: "E-Campus", description: "Plateforme de formation professionnelle pour les agents du Ministère de l'Intérieur" }, 
-    {title: "SPOTE", description: "Plateforme numérique collaborative des offres numériques de l'État" } ]
-
-
-exports.offres_fs_agents=[
-    {title:"Ministère de l’Économie, des Finances et de la Souveraineté industrielle et numérique*", url:"https://www.economie.gouv.fr/"},
-    {title:"Institut National de la Statistique et des Etudes Economiques (MEFSIN)", url:"https://www.insee.fr/fr/accueil"},
-    {title:"Direction générale des Finances publiques (MEFSIN)", url:"https://www.economie.gouv.fr/dgfip"},
-    {title:"Direction générale des Douanes et Droits indirects (MEFSIN)", url:"https://www.douane.gouv.fr/"},
-    {title:"Direction interministérielle du numérique (MEFSIN)", url:"https://www.numerique.gouv.fr/dinum/"},
-    {title:"Ministère de la Transition écologique et de la Cohésion des territoires", url:"https://www.ecologie.gouv.fr/"},
-    {title:"Ministère de la Transition énergétique", url:"https://www.ecologie.gouv.fr/"},
-    {title:"CEREMA", description: "Centre d’études et d’expertise sur les risques, l’environnement, la mobilité et l’aménagement", url:"https://www.cerema.fr/fr"},
-    {title:"INRAE", description: "Institut national de recherche pour l’agriculture, l’alimentation et l’environnement", url:"https://www.inrae.fr/"},
-]
+exports.offres_fs_agents = [
+  {
+    title:
+      "Ministère de l’Économie, des Finances et de la Souveraineté industrielle et numérique*",
+    url: "https://www.economie.gouv.fr/",
+  },
+  {
+    title:
+      "Institut National de la Statistique et des Etudes Economiques (MEFSIN)",
+    url: "https://www.insee.fr/fr/accueil",
+  },
+  {
+    title: "Direction générale des Finances publiques (MEFSIN)",
+    url: "https://www.economie.gouv.fr/dgfip",
+  },
+  {
+    title: "Direction générale des Douanes et Droits indirects (MEFSIN)",
+    url: "https://www.douane.gouv.fr/",
+  },
+  {
+    title: "Direction interministérielle du numérique (MEFSIN)",
+    url: "https://www.numerique.gouv.fr/dinum/",
+  },
+  {
+    title:
+      "Ministère de la Transition écologique et de la Cohésion des territoires",
+    url: "https://www.ecologie.gouv.fr/",
+  },
+  {
+    title: "Ministère de la Transition énergétique",
+    url: "https://www.ecologie.gouv.fr/",
+  },
+  {
+    title: "CEREMA",
+    description:
+      "Centre d’études et d’expertise sur les risques, l’environnement, la mobilité et l’aménagement",
+    url: "https://www.cerema.fr/fr",
+  },
+  {
+    title: "INRAE",
+    description:
+      "Institut national de recherche pour l’agriculture, l’alimentation et l’environnement",
+    url: "https://www.inrae.fr/",
+  },
+];
--- src/index.js
+++ src/index.js
@@ -1,43 +1,87 @@
-const express = require('express')
-var favicon = require('serve-favicon')
-const path = require('path')
-const dotenv = require('dotenv')
-const app = express()
-const { offres_fi_test, offres_fi, offres_fs_agents } = require("./datas-offres")
+const express = require("express");
+var favicon = require("serve-favicon");
+const path = require("path");
+const dotenv = require("dotenv");
+const app = express();
+const {
+  offres_fi_test,
+  offres_fi,
+  offres_fs_agents,
+} = require("./datas-offres");
 
 const routes = [
-  { path: '/', template: 'index', data: {offres_fs_agents, title: "Agent - AgentConnect"} },
-  { path: '/fi', template: 'fi', data: {offres_fi_test, offres_fi, title: "Fournisseur d'Identité - AgentConnect"} },
-  { path: '/fs', template: 'fs', data: {title: "Fournisseur de Services - AgentConnect"} },
-  { path: '/route', template: 'route', data: {title: "Feuille de route - AgentConnect"} },
-  { path: '/aide', template: 'aide',  data: {title: "Aide - AgentConnect"} },
-  { path: '/plan-du-site', template: 'plan-site',  data: {title: "Plan du site - AgentConnect"}},
-  { path: '/accessibilite', template: 'accessibilite',  data: {title: "Accessibilité - AgentConnect"}},
-  { path: '/mentions-legales', template: 'mentions-legales',  data: {title: "Mentions Légales - AgentConnect"}},
-  { path: '/cgu', template: 'cgu',  data: {title: "Conditions générales d'utilisation - AgentConnect"}},
-  { path: '/stats', template: 'stats',  data: {title: "Statistiques - AgentConnect"}},
+  {
+    path: "/",
+    template: "index",
+    data: { offres_fs_agents, title: "Agent - AgentConnect" },
+  },
+  {
+    path: "/fi",
+    template: "fi",
+    data: {
+      offres_fi_test,
+      offres_fi,
+      title: "Fournisseur d'Identité - AgentConnect",
+    },
+  },
+  {
+    path: "/fs",
+    template: "fs",
+    data: { title: "Fournisseur de Services - AgentConnect" },
+  },
+  {
+    path: "/route",
+    template: "route",
+    data: { title: "Feuille de route - AgentConnect" },
+  },
+  { path: "/aide", template: "aide", data: { title: "Aide - AgentConnect" } },
+  {
+    path: "/plan-du-site",
+    template: "plan-site",
+    data: { title: "Plan du site - AgentConnect" },
+  },
+  {
+    path: "/accessibilite",
+    template: "accessibilite",
+    data: { title: "Accessibilité - AgentConnect" },
+  },
+  {
+    path: "/mentions-legales",
+    template: "mentions-legales",
+    data: { title: "Mentions Légales - AgentConnect" },
+  },
+  {
+    path: "/cgu",
+    template: "cgu",
+    data: { title: "Conditions générales d'utilisation - AgentConnect" },
+  },
+  {
+    path: "/stats",
+    template: "stats",
+    data: { title: "Statistiques - AgentConnect" },
+  },
 ];
 
-dotenv.config()
+dotenv.config();
 
 app.set("views", path.join(__dirname, "../views"));
 app.set("view engine", "ejs");
-app.use(express.static('assets'));
+app.use(express.static("assets"));
 
 // Définition des routes
-routes.forEach(route => {
+routes.forEach((route) => {
   app.get(route.path, (req, res) => {
-    let data = route.data
-    data.url = req.url
+    let data = route.data;
+    data.url = req.url;
     res.render(route.template, data);
   });
 });
 
-app.use(express.static('public'));
-app.use("/static", express.static('node_modules/@gouvfr/dsfr/dist'));
+app.use(express.static("public"));
+app.use("/static", express.static("node_modules/@gouvfr/dsfr/dist"));
 
-app.use(favicon(path.join(__dirname, '../public/favicon', 'favicon.svg')))
+app.use(favicon(path.join(__dirname, "../public/favicon", "favicon.svg")));
 
 app.listen(process.env.PORT, () => {
-  console.log(`Vous êtes sur le port ${process.env.PORT}`)
-})
+  console.log(`Vous êtes sur le port ${process.env.PORT}`);
+});
